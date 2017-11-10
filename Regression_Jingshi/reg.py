e38a43ac1e93d3e95b2c123d087a7e193aedffd4
Jingshi <js4479@georgetown.edu>
2017-11-10 09:04:13 -0500

    update 10 fold cross validateion

--- Regression_Jingshi/reg.py
+++ Regression_Jingshi/reg.py
@@ -6,33 +6,73 @@ from preprocess import *
 from feature_factory import *
 from sklearn.neural_network import MLPRegressor
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
 
-#reg = GradientBoostingRegressor(n_estimators=200)
-reg = MLPRegressor(hidden_layer_sizes=[100, 50], activation='logistic')
+XGboost = GradientBoostingRegressor(n_estimators=200)
+MLP = MLPRegressor(hidden_layer_sizes=[100, 50], activation='logistic')
 
 for _emotion in ['anger', 'fear', 'joy', 'sadness']:
+	print ('')
+	print ('Emotion:', _emotion)
 	train_x, train_y = load_reg(emotion=_emotion)
 	dev_x, dev_y = load_reg(path='./data/2018-EI-reg-En-dev', emotion=_emotion)
-	
+
 	cdict = build_dict_from_corpus(train_x, min_freq=5)
 	train_x = lexicon_feature(train_x, cdict)
-	dev_x = lexicon_feature(dev_x, cdict)
+	#dev_x = lexicon_feature(dev_x, cdict)
+
+	print ('training data has', len(train_x), 'samples')
+	print ('XGBoost regressor')
+#	kfold = 0
+#	for idx1, idx2 in StratifiedKFold(n_splits=10).split(train_x, train_y):
+#		kfold += 1
+#		XGboost.fit(train_x[idx1,:], train_y[idx1])
+#		print ('** ', kfold, 'fold: pearson coef = ', measure_reg(train_y[idx2], XGboost.predict(train_x[idx2,:])))
+#		print ('** ', 'testing coef = ', measure_reg(dev_y, XGboost.predict(dev_x)))
+#
+#	print ('MLP regressor')
+#	kfold = 0
+#	for idx1, idx2 in StratifiedKFold(n_splits=10).split(train_x, train_y):
+#		kfold += 1
+#		MLP.fit(train_x[idx1,:], train_y[idx1])
+#		print ('** ', kfold, 'fold: pearson coef = ', measure_reg(train_y[idx2], MLP.predict(train_x[idx2,:])))
+#		print ('** ', 'testing coef = ', measure_reg(dev_y, MLP.predict(dev_x)))
+		
+	#folds
 	
-	reg.fit(train_x, train_y)
-	z = reg.predict(dev_x)
 	
-	print (measure_reg(dev_y, z))
+	kf = KFold(n_splits=10, random_state=2)
+	folds = kf.split(train_x, train_y)
+	kfold = 0
+	for idx1, idx2 in folds:
+		#print (idx1, idx2)
+		#training data
+		training_input = [train_x[i] for i in idx1]
+		training_output = [train_y[i] for i in idx1]
+		#test data
+		test_input = [train_x[i] for i in idx2]
+		test_output = [train_y[i] for i in idx2]
 
-# XGBoost
-#0.372739715258
-#0.403489378863
-#0.464128258254
-#0.436785900773
+		kfold += 1
+		XGboost.fit(training_input, training_output)
+		print ('** ', kfold, 'fold: ')
+		measure_reg(test_output, XGboost.predict(test_input))
+	
+	print ('MLP regressor')
+	kf = KFold(n_splits=10, random_state=2)
+	folds = kf.split(train_x, train_y)
+	kfold = 0
+	for idx1, idx2 in folds:
+		#print (idx1, idx2)
+		#training data
+		training_input = [train_x[i] for i in idx1]
+		training_output = [train_y[i] for i in idx1]
+		#test data
+		test_input = [train_x[i] for i in idx2]
+		test_output = [train_y[i] for i in idx2]
 
-
-# MLP
-#0.292609947712
-#0.392031740267
-#0.457844795001
-#0.435065708575
-
+		kfold += 1
+		MLP.fit(training_input, training_output)
+		print ('** ', kfold, 'fold: ')
+		measure_reg(test_output, MLP.predict(test_input))
--- Regression_Jingshi/utils.py
+++ Regression_Jingshi/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn.metrics import recall_score, confusion_matrix
 
+import scipy.stats
+
 def measure_clf(y, z):
 	print ('accuracy = ', np.mean(y == z))	
 	print ('micro-recall = ', recall_score(y, z, average='micro'))	
@@ -8,5 +10,7 @@ def measure_clf(y, z):
 	print ('confusion matrix = ', confusion_matrix(y, z))
 
 def measure_reg(y, z):
-	return np.corrcoef(y, z)[0,1]
-
+#	return np.corrcoef(y, z)[0,1]
+	pears_corr = scipy.stats.pearsonr(y, z)[0]
+	spear_corr = scipy.stats.spearmanr(y, z)[0]
+	print("Pearson correlation: " + str(pears_corr) + " Spearman correlation: " + str(spear_corr))
